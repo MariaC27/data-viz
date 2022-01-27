d48273ff3b8e342e72dae5ca2edee9b3f13b0b40
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2022-01-27 16:44:45 -0500

    print and check data

--- src/readdata.py
+++ src/readdata.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import matplotlib as mp
 
 data = pd.read_csv("/Users/Maria Cristoforo/DALI/DataVizCourse/myapp/src/data/global_food_prices.csv")
+# print(data)
 
-print(data)
+#check for missing data / mixed data types
+#print(data.isna().sum())
+
+#check for duplicates - none
+# print(data.duplicated())
+# print(data.columns.duplicated())
