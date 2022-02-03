4d601eec7fd4415b33bc6e928e3717e189e1562f
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2022-02-03 12:38:52 -0500

    data filtering in python

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import './App.css';
-import React, { useState, useEffect, Component } from 'react';
+// import './App.css';
+// import React, { useState, useEffect, Component } from 'react';
+// import * as d3 from 'd3';
+// import data from "./data/global_food_prices.csv";
+
+// class App extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       data: []
+//     };
+//   }
+
+//   componentDidMount(){
+//     d3.csv(data).then(function(d, error){
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log(d);
+//       };
+//     });
+//   }
+
+//   render(){
+//     return(
+//       <div>{this.state.data}</div>
+//     );
+//   }
+// }
+
+// export default App;
+
+//NOW LET'S USE REACT INSTEAD OF JUST D3 to load in data
+import React, { useState, useCallback, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import * as d3 from 'd3';
-import data from "./data/global_food_prices.csv";
+import { doExpression } from '@babel/types';
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={};
-  }
+const csvUrl =
+'https://media.githubusercontent.com/media/MariaC27/data-viz/main/src/data/global_food_prices.csv';
 
-  componentDidMount(){
-    d3.csv(data).then(function(d, error){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(d);
-      };
-    });
-  }
+const message = data => {
+  let message = '';
+  message = message + d3.csvFormat(data).length / 1024 + ' kB\n';
+  message = message + data.length + ' rows\n';
+  message = message + data.columns.length + ' columns';
+  return message; 
+};
 
-  render(){
-    return(
-      <div>test</div>
-    );
-  }
-}
+const App = () => {
+  //look, these are states just like useMousePositions
+  const [data, setData] = useState(null); //intial data is just null.
+
+  //If we didn't use useEffect, d3.csv(csvUrl).then(setData would run infinitely. that's what the empty array indicates.
+  //the then here simply takes the CSV and populated setData with it when it runs. UseEffect makes ir tun only once
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData);
+  }, []);
+
+  return (message(data));
+};
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
 
 export default App;
--- src/readdata.py
+++ src/readdata.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import matplotlib as mp
 
-data = pd.read_csv("/Users/Maria Cristoforo/DALI/DataVizCourse/myapp/src/data/global_food_prices.csv")
-# print(data)
+col_list = ["adm0_name", "cm_name", "cur_name", "mp_year", "mp_price"]
+subset_columns = ["adm0_name", "cm_name"]
+chosen_rows = ["Afghanistan", "China", "Columbia", "Egypt", "Lebanon"]
+data = pd.read_csv("/Users/Maria Cristoforo/DALI/DataVizCourse/myapp/src/data/global_food_prices.csv", usecols=col_list)
+data = data.drop_duplicates(subset=subset_columns)
+
+# row_names = data["adm0_name"].drop_duplicates()
+# print(row_names)
+
+#print(data)
+
+data.to_csv('filtered-data.csv')
+
 
 #check for missing data / mixed data types
-print(data.isna().sum())
+# print(data.isna().sum())
 
-#check for duplicates - none
-print(data.duplicated())
-print(data.columns.duplicated())
+# #check for duplicates - none
+# print(data.duplicated())
+# print(data.columns.duplicated())
